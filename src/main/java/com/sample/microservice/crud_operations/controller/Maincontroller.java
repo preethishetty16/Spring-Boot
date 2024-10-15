59658d711ec31fc224970b390a7aef134372e2bf
Preethi Shetty <preethikurdune@gmail.com>
2024-10-15 20:00:27 +0530

    Update employee endpoint added

--- src/main/java/com/sample/microservice/crud_operations/controller/Maincontroller.java
+++ src/main/java/com/sample/microservice/crud_operations/controller/Maincontroller.java
@@ -3,7 +3,9 @@ package com.sample.microservice.crud_operations.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,29 @@ public class Maincontroller {
 			@RequestBody @Valid EmployeeCreateRequest emloyeeCreateRequest ){
 		
 		EmployeeCreateResponse response= service.createEmployee(emloyeeCreateRequest);
+		ServiceMessage serviceMessage;
+		HttpStatus httpStatus;
+		if(null == response) {
+			serviceMessage = ServiceMessage.createDuplicateDataServiceMessage();
+			httpStatus= HttpStatus.CONFLICT;
+			serviceMessage.getErrors().add(new ValidatorErrorResponse(null, "CODE","EMPLOYEE EXISTS"));
+		}else {
+			serviceMessage = ServiceMessage.successMessageForCreated();
+			httpStatus= HttpStatus.CREATED;
+			serviceMessage.getMessages().add(new ValidatorErrorResponse(null,"CODE","EMPLOYEE CREATED"));
+		}
+				return new ResponseEntity<>(Response.createResponse(serviceMessage, response), httpStatus);
+		
+	}
+	
+	@PutMapping(value="/update/{id}")
+	public ResponseEntity<Response<EmployeeCreateResponse>> editEmployee(
+			@RequestHeader(name="Country" , required= true) String country,
+			@RequestHeader(name ="UserId", required = true) String userId,
+			@PathVariable Integer id,
+			@RequestBody @Valid EmployeeCreateRequest emloyeeCreateRequest ){
+		
+		EmployeeCreateResponse response= service.updateEmployee(id, emloyeeCreateRequest);
 		ServiceMessage serviceMessage;
 		HttpStatus httpStatus;
 		if(null == response) {
--- src/main/java/com/sample/microservice/crud_operations/service/EmployeeService.java
+++ src/main/java/com/sample/microservice/crud_operations/service/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.sample.microservice.crud_operations.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,27 +13,39 @@ import com.sample.microservice.crud_operations.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-	
+
 	@Autowired
 	private EmployeeRepository repository;
 
 	public EmployeeCreateResponse createEmployee(EmployeeCreateRequest request) {
-		
-		Employee e= new Employee();
+
+		Employee e = new Employee();
 		e.setEmployeeDepartment(request.getEmployeeDepartment());
 		e.setEmailId(request.getEmailId());
 		e.setEmployeeId(request.getEmployeeId());
 		e.setEmployeeName(request.getEmployeeName());
 		e.setEmployeeSalary(request.getEmployeeSalary());
-		
+
 		if (repository.existsByEmailId(e.getEmailId())) {
-	        throw new DuplicateRecordException("Email already exists", null, null);
-	    }
+			throw new DuplicateRecordException("Email already exists", null, null);
+		}
 		if (repository.existsByEmployeeId(e.getEmployeeId())) {
-	        throw new DuplicateRecordException("Emloyee ID already exists", null, null);
-	    }
-		
-		Employee response= repository.save(e);
+			throw new DuplicateRecordException("Emloyee ID already exists", null, null);
+		}
+
+		Employee response = repository.save(e);
+		return new EmployeeCreateResponse(response.getEmployeeId());
+	}
+
+	public EmployeeCreateResponse updateEmployee(Integer id, EmployeeCreateRequest updatedEmployee) {
+		Employee employee = repository.findByEmployeeId(id);
+		if (employee == null) {
+			throw new DuplicateRecordException("Emloyee ID doesn't exist", null, null);
+		}
+		employee.setEmployeeName(updatedEmployee.getEmployeeName());
+		employee.setEmployeeSalary(updatedEmployee.getEmployeeSalary());
+		employee.setEmployeeDepartment(updatedEmployee.getEmployeeDepartment());
+		Employee response =  repository.save(employee);
 		return new EmployeeCreateResponse(response.getEmployeeId());
 	}
 
